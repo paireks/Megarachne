28f0dd20e3ea26004daf3e222878057db6116eec
paireks <47977819+paireks@users.noreply.github.com>
2020-12-03 19:58:13 +0100

    Initial commit

---
+++ Megarachne/Megarachne/MegarachneInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Megarachne
+{
+    public class MegarachneInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Megarachne";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                return "Graph theory plugin for Grasshopper";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("dbefe6f6-50df-458b-a32b-61a514b3f101");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "code-structures Wojciech Radaczyński";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+        public override string Version
+        {
+            get
+            {
+                return "1.0.0.0";
+            }
+        }
+    }
+}
---
+++ Megarachne/Megarachne/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Megarachne")]
+[assembly: AssemblyDescription("Graph theory plugin for Grasshopper")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Megarachne")]
+[assembly: AssemblyCopyright("Copyright © code-structures Wojciech Radaczynski  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f764f67-22c3-4d3e-b3b7-abb696a81c9f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
