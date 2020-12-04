10ec9b5ee328d0400b2811133a99ce0abb328929
paireks <47977819+paireks@users.noreply.github.com>
2020-12-04 21:25:55 +0100

    Start of engine

---
+++ Megarachne/MegarachneEngine/Graph.cs
@@ -0,0 +1,28 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+
+namespace MegarachneEngine
+{
+    public class Graph
+    {
+        public Graph(List<GraphPart> graphParts, double tolerance)
+        {
+            Vertices = new List<Point3d>();
+            Edges = new Curve[graphParts.Count];
+            GraphArray = new int[graphParts.Count, 2];
+
+            for (int i = 0; i < graphParts.Count; i++)
+            {
+                Edges[i] = graphParts[i].Edge;
+
+                
+            }
+
+        }
+
+        public int[,] GraphArray { get; set; }
+        public Curve[] Edges { get; set; }
+        public List<Point3d> Vertices { get; set; }
+    }
+}
---
+++ Megarachne/MegarachneEngine/GraphPart.cs
@@ -0,0 +1,43 @@
+﻿using Rhino.Geometry;
+
+namespace MegarachneEngine
+{
+    public class GraphPart
+    {
+        public GraphPart(Curve curve, bool isDirected)
+        {
+            StartVertex = curve.PointAtStart;
+            EndVertex = curve.PointAtEnd;
+            Edge = curve;
+            IsDirected = isDirected;
+        }
+
+        public GraphPart(Vector3d vector, Point3d point, bool isDirected)
+        {
+            StartVertex = point;
+            EndVertex = point + vector;
+
+            Line edgeLine = new Line(point, vector);
+            Edge = new LineCurve(edgeLine);
+
+            IsDirected = isDirected;
+        }
+
+        public GraphPart(Point3d pointA, Point3d pointB, bool isDirected)
+        {
+            StartVertex = pointA;
+            EndVertex = pointB;
+
+            Line edgeLine = new Line(pointA, pointB);
+            Edge = new LineCurve(edgeLine);
+
+            IsDirected = isDirected;
+        }
+
+        public bool IsDirected { get; set; }
+        public Point3d StartVertex { get; set; }
+        public Point3d EndVertex { get; set; }
+        public Curve Edge { get; set; }
+
+    }
+}
