4cb579fd91fc3f1124806851ee4e885fb2f727ec
paireks <47977819+paireks@users.noreply.github.com>
2020-12-28 03:54:51 +0100

    BFS

---
+++ Megarachne/Megarachne/BFSShortestPathGH.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using MegarachneEngine;
+using Rhino.Geometry;
+
+namespace Megarachne
+{
+    public class BfsShortestPathGH : GH_Component
+    {
+        public BfsShortestPathGH()
+          : base("BFS Shortest Path", "BFS Shortest Path",
+              "Returns Breadth First Search's shortest path as a list of ordered vertices",
+              "Megarachne", "Algorithm")
+        {
+        }
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Graph", "Graph", "Input graph", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Start Vertex", "Start Vertex", "Index of start vertex",
+                GH_ParamAccess.item);
+            pManager.AddIntegerParameter("End Vertex", "End Vertex", "Index of end vertex", GH_ParamAccess.item);
+        }
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddIntegerParameter("Vertices List", "Vertices List",
+                "List of ordered vertices as the shortest path", GH_ParamAccess.list);
+        }
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Graph graph = null;
+            int startVertexIndex = 0;
+            int endVertexIndex = 0;
+
+            DA.GetData(0, ref graph);
+            DA.GetData(1, ref startVertexIndex);
+            DA.GetData(2, ref endVertexIndex);
+
+            DA.SetDataList(0, Algorithm.BfsShortestPath(graph, startVertexIndex, endVertexIndex));
+        }
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("7c3906f8-861b-41f5-8fc9-abceca13dfc9"); }
+        }
+    }
+}
---
+++ Megarachne/MegarachneEngine/Algorithm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegarachneEngine
+{
+    public static class Algorithm
+    {
+        public static void Bfs(Graph graph)
+        {
+            List<int>[] adjacencyList = graph.AdjacencyList;
+            bool[] visited = new bool[graph.Vertices.Count];
+            Queue<int> queue = new Queue<int>();
+            int startVertex = 0;
+
+            queue.Enqueue(startVertex);
+            visited[startVertex] = true;
+            while (queue.Count != 0)
+            {
+                int vertex = queue.Dequeue();
+
+                foreach (int neighbor in adjacencyList[vertex])
+                {
+                    if (visited[neighbor])
+                    {
+                        continue;
+                    }
+                    queue.Enqueue(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+        public static List<int> BfsShortestPath(Graph graph, int startVertexIndex, int endVertexIndex)
+        {
+            List<int>[] adjacencyList = graph.AdjacencyList;
+            bool[] visited = new bool[graph.Vertices.Count];
+            Queue<int> queue = new Queue<int>();
+            int startVertex = startVertexIndex;
+            int[] previous = new int[graph.Vertices.Count];
+
+            queue.Enqueue(startVertex);
+            visited[startVertex] = true;
+
+            bool keepSearching = true;
+            while (queue.Count != 0 || keepSearching)
+            {
+                int vertex = queue.Dequeue();
+
+                foreach (int neighbor in adjacencyList[vertex])
+                {
+                    if (visited[neighbor])
+                    {
+                        continue;
+                    }
+                    queue.Enqueue(neighbor);
+                    visited[neighbor] = true;
+                    previous[neighbor] = vertex;
+                    if (neighbor == endVertexIndex)
+                    {
+                        keepSearching = false;
+                        break;
+                    }
+                }
+            }
+
+            List<int> shortestPath = new List<int>();
+
+            shortestPath.Add(endVertexIndex);
+
+            int currentVertex = endVertexIndex;
+
+            while (previous[currentVertex] != startVertexIndex)
+            {
+                shortestPath.Add(previous[currentVertex]);
+                currentVertex = previous[currentVertex];
+            }
+
+            shortestPath.Add(startVertexIndex);
+
+            shortestPath.Reverse();
+
+            return shortestPath;
+        }
+
+    }
+}
